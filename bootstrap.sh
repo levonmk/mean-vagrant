6f019e06a1b452a730c17e4b81b67830e6fa6fef
Levon K. Mkrtchyan <levon.mkrtchyan@gmail.com>
2014-06-22 16:28:58 -0400

    adding more dependencies to shell script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,14 @@
 
 #basics
 apt-get update
+apt-get install -y git
+apt-get install -y npm
+apt-get install -y curl
+apt-get install -y make
+apt-get install -y build-essential
+apt-get install -y libssl-dev
 apt-get install -y byobu
 apt-get install -y vim
-apt-get install -y npm
 
 #mongodb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
